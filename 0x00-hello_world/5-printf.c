b787f9a4609f1812b6be1ef1406e194edfc8a79d
frontlinecoder <zachdutic@gmail.com>
2022-12-08 14:06:30 -0800

    Print a c String

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+/*
+ * main - Prints with proper grammar, but the outcome is a piece of art.
+ *
+ * Return: always 0.
+ *
+ */
+int main()
+{
+char ch[]="with proper grammar, but the outcome is a piece of art,";	
+
+putchar(ch);
+
+return 0;
+}
