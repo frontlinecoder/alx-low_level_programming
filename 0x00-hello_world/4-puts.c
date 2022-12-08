1c45db7ddbcd6226f301044f8150265470acac38
frontlinecoder <zachdutic@gmail.com>
2022-12-08 13:14:22 -0800

    Print a c quote

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 
-/*
- * main - A C program that prints exactly Programming is like building a
- * multillingual puzzle, followed by a new line.
+/**
+ *main - Prints Programming is like building a multilingual puzzle, followed by
+ * a new line.
  */
 int main(void)
 {
