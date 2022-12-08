8bc02a68aa871ea549afbf92792161491425678a
frontlinecoder <zachdutic@gmail.com>
2022-12-08 15:11:28 -0800

    size of various types

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 
-/*
+/**
  *main - Prints "with proper grammar, but the outcome is a piece of art,"
  *followed by a new line.
  *
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,19 +1,19 @@
 #include<stdio.h>
 
-/*main - Prints A c program that prints the size of various types on the computer it is compiled and run on.*/
+/**
+ * main - Prints A c program that prints the size of various types on the computer it is compiled and run on.
+ *
+ *Return: Always 0;
+ *
+ * */
 
-/*Return: Always 0;*/
 
 int main(void)
 {
 printf("Size of a char: %zu byte(s)\n", sizeof(char));
-
 printf("Size of an int: %zu byte(s)\n", sizeof(int));
-
 printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
-
 printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
-
 printf("Size of a float: %zu byte(s)\n", sizeof(float));
 
 return (0);
