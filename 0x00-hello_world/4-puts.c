1f258b8b2695e60b2391cfa7791e94561e6bcc39
frontlinecoder <zachdutic@gmail.com>
2022-12-08 12:55:21 -0800

    Print a c quote

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include<stdio.h>
-
-
+/*
+ * a C program that prints exactly Programming is like building a multilingual 
+ * puzzle, followed by a new line.
+*/
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
-        return (0);
+	return (0);
 }
