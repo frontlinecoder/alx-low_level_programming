b44c64852e33505b390e9ca62507764417d57df4
frontlinecoder <zachdutic@gmail.com>
2022-12-08 12:46:02 -0800

    Print a c quote

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include<stdio.h>
-int puts(){
-if(printf("\"Programming is like building a multilingual puzzle\n")){
 
-}
-return 0;
+
+int main(void)
+{
+        puts("\"Programming is like building a multilingual puzzle");
+
+        return (0);
 }
