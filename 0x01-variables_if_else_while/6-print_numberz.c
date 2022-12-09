73bf9fa38f754f21179561c1fd3ae0f501cd8ae2
frontlinecoder <zachdutic@gmail.com>
2022-12-09 14:57:04 -0800

    Prints digit numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Prints digit numbers without a char variable.
+ *
+ * Return: always 0.
+ */
+
+int main(void)
+{
+
+int num;
+
+for (num = 0; num < 10; num++)
+putchar((num % 10) + '0');
+
+putchar('\n');
+
+return (0);
+	
+}
