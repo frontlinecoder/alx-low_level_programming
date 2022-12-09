6d86fcc1a1d7dc759e374d78f3caa8561e37bc6f
frontlinecoder <zachdutic@gmail.com>
2022-12-09 14:40:05 -0800

    prints the letters into uppercase

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase.
+ *
+ * Return: always 0.
+ */
+
+int main(void)
+{
+
+char letter;
+
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+
+if (letter != 'e' && letter != 'q')
+putchar(letter);
+
+}
+putchar('\n');
+
+return (0);
+}
