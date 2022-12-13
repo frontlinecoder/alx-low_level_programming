26d293c94c654e31619e4432089bea1ec25112d2
frontlinecoder <zachdutic@gmail.com>
2022-12-13 13:58:05 -0800

    Combination of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -16,13 +16,10 @@ for (num1 = 0; num1 < 9; num1++)
 	for (num2 = num1 + 1; num2 < 10; num2++ )
 
 	{
-
 		putchar((num1 % 10) + ‘0’);
 		putchar((num2 % 10) + ‘0’);
 
 		if (num1 == 8 && num2 == 9)
-			continue;
-
 		putchar(‘,’);
 		putchar(‘ ’);
 
