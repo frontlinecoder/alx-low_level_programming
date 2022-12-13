464e62d7acc0a48aa37fe42143d3687aba1f4944
frontlinecoder <zachdutic@gmail.com>
2022-12-13 15:57:42 -0800

    Combination of two digits

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,30 +9,43 @@ int main(void)
 {
 int i, j;
 
-
-
 for (i = 0; i < 100; i++)
-			
-{
-
-for (j = 0; j < 100; j++)
 
 {
-										if (i < j)
-{									
-putchar((i / 10) + 48);
-										putchar((i % 10) + 48);
-										putchar(' ');																			putchar((j / 10) + 48);
 
-putchar((j % 10) + 48);
+	for (j = 0; j < 100; j++)
 
-if (i != 98 || j != 99)
-{
-putchar(',');									putchar(' ');
-										}
-}
-}
-}
+	{
+
+		if (i < j)
+
+		{
+
+			putchar((i / 10) + 48);
+
+			putchar((i % 10) + 48);
+
+			putchar(' ');
+
+			putchar((j / 10) + 48);
+
+			putchar((j % 10) + 48);
+
+			if (i != 98 || j != 99)
+
+			{
+
+				putchar(',');
+
+				putchar(' ');
+
+			}
+
+		}
+
+	}
+
+}	
 
 putchar('\n');
 
