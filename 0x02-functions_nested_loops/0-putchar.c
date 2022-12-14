a588aad67524d1e041c153232f493f8d04f19139
frontlinecoder <zachdutic@gmail.com>
2022-12-14 12:24:49 -0800

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Prints _putchar
+ *
+ * Retur - always 0.
+ */
+int main(void)
+{
+
+_putchar('_');
+
+_putchar('p');
+
+_putchar('u');
+
+_putchar('t');
+
+_putchar('c');
+
+_putchar('h');
+
+_putchar('a');
+
+_putchar('r');
+
+_putchar('\n');
+
+return 0;
+
+}
