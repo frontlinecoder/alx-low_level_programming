26bb21c8babdc88fb761d6dd8519c310e380d41b
frontlinecoder <zachdutic@gmail.com>
2022-12-15 01:17:19 -0800

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,7 +3,7 @@
 /**
  * main - Prints _putchar
  *
- * Retur - always 0.
+ * Return - always 0.
  */
 int main(void)
 {
@@ -25,6 +25,6 @@ putchar('r');
 
 putchar('\n');
 
-return 0;
+return (0);
 
 }
