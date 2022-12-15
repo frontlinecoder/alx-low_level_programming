89ca1997f653cb22072436d533ae1cd416a7606d
frontlinecoder <zachdutic@gmail.com>
2022-12-15 03:50:51 -0800

    9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,83 +6,42 @@
  *
  * Return: 0 always succes
  */
-
 void times_table(void)
-
 {
-
 	int a, b, c;
-
 	for (a = 0; a <= 9; a++)
-
 	{
-
 		for (b = 0; b <= 9; b++)
-
 		{
-
 			c = a * b;
-
 			if ((c / 10) == 0)
-
 			{
-
 				if (b == 0)
-
 				{
-
 					putchar('0');
-
 				}
-
 				if (b != 0)
-
 				{
-
 					putchar(' ');
-
 					putchar((c % 10) + '0');
-
 				}
-
 				if (b < 9)
-
 				{
-
 					putchar(',');
-
 					putchar(' ');
-
 				}
-
 			}
-
 			else
-
 			{
-
 				putchar((c / 10) + '0');
-
 				putchar((c % 10) + '0');
-
 				if (b < 9)
-
 				{
-
 					putchar(',');
-
 					putchar(' ');
-
 				}
-
 			}
-
 		}
-
 		putchar('\n');
-
 	}
-
 }
-
-
