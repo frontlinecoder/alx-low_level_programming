7bffe341f38cdddb30b64ad2657fceb806591683
frontlinecoder <zachdutic@gmail.com>
2022-12-15 01:44:26 -0800

    Prints lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * print_alphabet - lowercase.
+ *
+ *
+ * Return: always 0.
+ */
+
+void print_alphabet(void)
+
+{
+
+	char c = 'a';
+
+	while (c <= 'z')
+
+	{
+
+		_putchar(c);
+
+		c++;
+
+	}
+
+	_putchar('\n');
+
+	Return (0);
+
+}
