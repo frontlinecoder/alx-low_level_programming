808c0fe9acb0e6180d4f6b645dd36a169c960478
frontlinecoder <zachdutic@gmail.com>
2022-12-15 04:02:27 -0800

    adds two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * add - Entry point for the addition
+ * @a: one value to be added together
+ * @b: second value to be added
+ * Return: Always 0.
+ */
+
+int add(int a, int b)
+
+{
+
+	return (a + b);
+
+}
+
