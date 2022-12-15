5b6af299e27f44335f00cc2466a1f05cde0f5eab
frontlinecoder <zachdutic@gmail.com>
2022-12-15 01:13:54 -0800

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,23 +7,23 @@
  */
 int main(void)
 {
-_putchar('_');
+putchar('_');
 
-_putchar('p');
+putchar('p');
 
-_putchar('u');
+putchar('u');
 
-_putchar('t');
+putchar('t');
 
-_putchar('c');
+putchar('c');
 
-_putchar('h');
+putchar('h');
 
-_putchar('a');
+putchar('a');
 
-_putchar('r');
+putchar('r');
 
-_putchar('\n');
+putchar('\n');
 
 return 0;
 
