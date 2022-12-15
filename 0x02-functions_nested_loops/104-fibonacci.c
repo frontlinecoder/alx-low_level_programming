4de374eca822ff58ffa080e9fdd05cc9baa7984d
frontlinecoder <zachdutic@gmail.com>
2022-12-15 09:35:30 -0800

    prints the first 98 Fibonacci numbers

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -40,42 +40,25 @@ int main(void)
 	{
 		if (f1o > 0)
 			printf("%lu", f1o);
-
 		initial0s = numLength(mx) - 1 - numLength(f1);
-
 		while (f1o > 0 && initial0s > 0)
 		{
 			printf("%i", 0);
 			initial0s--;
 		}
 		printf("%lu", f1);
-
 		tmp = (f1 + f2) % mx;
-
 		tmpo = f1o + f2o + (f1 + f2) / mx;
-
 		f1 = f2;
-
 		f1o = f2o;
-
 		f2 = tmp;
-
 		f2o = tmpo;
 
 		if (i != 98)
-
 			printf(", ");
-
 		else
-
 			printf("\n");
-
 		i++;
-
 	}
-
 	return (0);
-
 }
-
-
