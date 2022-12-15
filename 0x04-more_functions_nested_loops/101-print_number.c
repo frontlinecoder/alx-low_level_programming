8d9372040ceefff0317804be8f37d9d1e6be520b
frontlinecoder <zachdutic@gmail.com>
2022-12-15 11:49:54 -0800

    prints an integer.

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -3,34 +3,28 @@
 /**
  *print_number - Prints integer.
  *
- * @n: param
+ * @n: Digit
  */
-
 void print_number(int n)
+
 {
-	unsigned int i, j, count;
+
+	unsigned int num = n;
 
 	if (n < 0)
-	{
-		putchar(45);
-		i = n * -1;
-	}
 
-	else
-	{
-		i = n;
-	}
-	j = i
-	count = 1;
-
-	while (j > 9)
-	{
-		j /= 10;
-		count *= 10;
-	}
-	for (; count >= 1; count /= 10)
 	{
 
-		putchar(((i / count) % 10) + 48);
+		putchar('-');
+
+		num = -num;
+
 	}
+
+	if ((num / 10) > 0)
+
+		print_number(num / 10);
+
+	putchar((num % 10) + '0');
+
 }
