0b05c99b370df04d827542ed10eced9d19375693
frontlinecoder <zachdutic@gmail.com>
2022-12-15 04:14:15 -0800

    times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,85 +9,45 @@
 void print_times_table(int n)
 
 {
-
 	int row;
 
 	int column;
 
 	int product;
 
-
-
 	if (n < 0 || n >= 15)
-
 		return;
-
 	for (row = 0; row <= n; row++)
-
 	{
-
 		for (column = 0; column <= n; column++)
-
 		{
-
 			product = (row * column);
-
 			if (column == 0)
-
-				_putchar('0' + product);
-
+				putchar('0' + product);
 			else
-
 			{
-
-				_putchar(',');
-
-				_putchar(' ');
-
+				putchar(',');
+				putchar(' ');
 				if (product <= 9)
-
 				{
-
-					_putchar(' ');
-
-					_putchar(' ');
-
-					_putchar('0' + product);
-
+					putchar(' ');
+					putchar(' ');
+					putchar('0' + product);
 				}
-
 				else if (product > 9 && product < 100)
-
 				{
-
-					_putchar(' ');
-
-					_putchar('0' + (product / 10));
-
-					_putchar('0' + (product % 10));
-
+					putchar(' ');
+					putchar('0' + (product / 10));
+					putchar('0' + (product % 10));
 				}
-
 				else if (product >= 100)
-
 				{
-
-					_putchar('0' + (product / 100));
-
-					_putchar('0' + ((product / 10) % 10));
-
-					_putchar('0' + (product % 10));
-
+					putchar('0' + (product / 100));
+					putchar('0' + ((product / 10) % 10));
+					putchar('0' + (product % 10));
 				}
-
 			}
-
 		}
-
-		_putchar('\n');
-
+		putchar('\n');
 	}
-
 }
-
-
