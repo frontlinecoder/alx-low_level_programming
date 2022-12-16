b328d6afc21801683f7e1ed5a5c3403c626aeec7
frontlinecoder <zachdutic@gmail.com>
2022-12-16 13:22:26 -0800

    Task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * print_numbers - prints number.
+ */
+
+void print_numbers(void)
+
+{
+
+	int i;
+
+	for (i = 48; i < 58; i++)
+
+	{
+
+		putchar(i);
+
+	}
+
+	putchar('\n');
+
+}
+
+
