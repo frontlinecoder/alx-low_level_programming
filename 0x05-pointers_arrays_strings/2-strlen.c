909a7977319215d465339f2a39a99816d774740b
frontlinecoder <zachdutic@gmail.com>
2022-12-19 09:52:57 -0800

    Task 1

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+* _strlen - a function to print the length of a string
+*
+* @s: string to be supplied
+*
+* Return: Always 0
+*/
+
+int _strlen(char *s)
+
+{
+
+	int i = 0;
+
+	while (s[i] != '\0')
+
+	{
+
+		i++;
+
+	}
+
+	return (i);
+
+}
