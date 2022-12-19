6b1bb115fa70793660f1bbad4d999ca0ba5ef366
frontlinecoder <zachdutic@gmail.com>
2022-12-19 10:17:28 -0800

    Task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
 /**
-*
-* _puts - function that prints a string, followed by a new line, to stdout
+* _puts - function that prints a string.
 *
 * @str: pointer value
 *
-* Return: Always 0 (Success)
+* Return: Always 0.
 *
 */
 
