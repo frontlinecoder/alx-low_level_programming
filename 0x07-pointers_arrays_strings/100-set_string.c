5cb4216c4b555f3c3d670325294f6723e6cc2e34
frontlinecoder <zachdutic@gmail.com>
2023-01-04 07:16:12 -0800

    task 100

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+
+/**
+* set_string - set value to char
+* @s: double pointer
+* @to: pointer
+* Return: always 0
+*/
+
+void set_string(char **s, char *to)
+
+{
+
+	*s = to;
+
+}
