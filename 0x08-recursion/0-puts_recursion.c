8ee2f311e8d134d3823aab2aece10e215a72e56f
frontlinecoder <zachdutic@gmail.com>
2023-01-06 10:33:33 -0800

    task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+*_puts_recursion - prints a string
+*
+*@s: pointer block of memory to fill
+*Return: void
+*/
+
+void _puts_recursion(char *s)
+
+{
+
+	if (*s == '\0')
+
+	{
+
+		putchar('\n');
+
+		return;
+
+	}
+
+	putchar (*s);
+
+	puts_recursion(s + 1);
+
+}
