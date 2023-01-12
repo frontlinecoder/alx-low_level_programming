2a10fa792698aa3d753843beb1d4aa8560559d24
frontlinecoder <zachdutic@gmail.com>
2023-01-12 08:12:21 -0800

    task 0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* create_array - create arrays of char dynamically
+* @size: size
+* @c: always zero
+* Return: Always zero
+*/
+
+char *create_array(unsigned int size, char c)
+{
+	char *arrays;
+
+	unsigned int b;
+
+	if (size == 0)
+
+		return (NULL);
+
+	arrays = (char *)malloc(sizeof(char) * size);
+
+	if (arrays == NULL)
+
+		return (NULL);
+
+	for (b = 0; b < size; b++)
+
+		arrays[b] = c;
+	return (arrays);
+}
